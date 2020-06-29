c74e75c3341b661852aefc5acb52dfd09390d5a2
chen1116@gmail.com <@2irigiai>
2020-06-29 11:03:39 +0800

    update eg_webbrowser

--- Hung19/eg_webbrowser.py
+++ Hung19/eg_webbrowser.py
@@ -6,5 +6,10 @@ def open_url():
     webbrowser.open('http://www.yahoo.com.tw', 1)
 
 
+def google_map_lookup(location):
+    webbrowser.open("http://www.google.com.tw/maps/place/" + location)
+
+
 if __name__ == '__main__':
-    open_url()
+    # open_url()
+    google_map_lookup("漢神百貨")
