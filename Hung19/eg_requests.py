577f3aacd76441ce718799fcf0971c5f8a864ef3
chen1116@gmail.com <@2irigiai>
2020-06-29 15:19:05 +0800

    update requests

--- Hung19/eg_requests.py
+++ Hung19/eg_requests.py
@@ -15,5 +15,13 @@ def get_url(url):
         print("{} get file error".format(url))
 
 
+def get_url_with_exception(url):
+    html = requests.get(url)
+    try:
+        html.raise_for_status()
+    except Exception as err:
+        print("html get url: {}".format(url))
+
+
 if __name__ == '__main__':
     get_url("http://www.yahoo.com.tw")
